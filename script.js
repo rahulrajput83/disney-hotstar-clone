7cb6a15b8a5e89cdb9f7b6025a0c35692630ae6b
rahulrajput83 <rahulrajput21112@gmail.com>
2022-10-24 20:00:07 +0530

    Updated Cards

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let scroll = 0;
 
 btn.addEventListener('click', () => {
     ele.style.overflowX = 'scroll';
-    scroll += 100;
+    scroll += 400;
     console.log(scroll);
     ele.scrollTo(scroll, 0);
     /* setDefault(); */
